daf2970cb4334a06976cba2645c186a0565442ef
PetraNaibaho <petranaibaho5@gmail.com>
2024-09-06 10:50:00 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,1 +1,34 @@
-// NIM - Your Name
+// 12S24028 - Rotua Immanuela Tampubolon
+// 12S24002 - Petra Ignatius Pengayoman Naibaho
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN;
+        String jUDUL;
+        String pENULIS;
+        int tAHUN;
+        String pENERBIT;
+        String fORMATBUKU;
+        double hARGA;
+        double mARGIN;
+        int sTOK;
+        double rATING;
+
+        iSBN = input.nextLine();
+        jUDUL = input.nextLine();
+        pENULIS = input.nextLine();
+        tAHUN = input.nextInt();
+        pENERBIT = input.nextLine();
+        fORMATBUKU = input.nextLine();
+        hARGA = input.nextDouble();
+        mARGIN = input.nextDouble();
+        sTOK = input.nextInt();
+        rATING = input.nextDouble();
+        System.out.println(iSBN + "|" + jUDUL + "|" + pENULIS + "|" + tAHUN + "|" + pENERBIT + "|" + fORMATBUKU + "|" + hARGA + "|" + mARGIN + "|" + sTOK + "|" + rATING);
+    }
+}
+
